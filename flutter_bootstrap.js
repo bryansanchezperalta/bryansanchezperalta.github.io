ee1f13c0138e9fffa5222fe9f59e5ed89d3f3a9a
BlAcKPhOeNiX233 <bryansanchez.311@gmail.com>
2025-02-03 00:28:02 +0100

    Changed scroll paramiters

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2639435176"
+    serviceWorkerVersion: "2043009791"
   }
 });
