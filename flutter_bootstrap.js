6017d899663291ff3fbaea042fb283adbe5bbb7d
BlAcKPhOeNiX233 <bryansanchez.311@gmail.com>
2025-02-03 00:56:53 +0100

    Improved UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1383817711"
+    serviceWorkerVersion: "2124124269"
   }
 });
